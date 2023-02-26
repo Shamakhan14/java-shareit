140eb4ce257577d4a759e5be3ba78908561a22a5
Shamakhan <valentin-u-a@yandex.ru>
2023-02-26 03:57:05 +0300

    feat: Добавлен тест CommentRepository

--- src/test/java/ru/practicum/shareit/repository/CommentRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/repository/CommentRepositoryTest.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import ru.practicum.shareit.item.ItemRepository;
+import ru.practicum.shareit.item.CommentRepository;
+import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,18 +19,25 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @DataJpaTest
-public class ItemRepositoryTest {
+public class CommentRepositoryTest {
     @Autowired
     private TestEntityManager em;
     @Autowired
-    private ItemRepository repository;
+    private CommentRepository repository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
-    void findByOwnerOrderByIdPageableTest() {
+    void findByItemInTest() {
         User user = new User();
         user.setName("name");
         user.setEmail("email@email.com");
         em.persist(user);
+
+        User newUser = new User();
+        newUser.setName("newName");
+        newUser.setEmail("newEmail@email.com");
+        em.persist(newUser);
 
         Item item = new Item();
         item.setName("name");
@@ -39,15 +46,20 @@ public class ItemRepositoryTest {
         item.setOwner(user.getId());
         em.persist(item);
 
-        Pageable pageable = PageRequest.of(0, 20, Sort.by(DESC, "id"));
+        Comment comment = new Comment();
+        comment.setText("text");
+        comment.setItem(item.getId());
+        comment.setAuthor(newUser);
+        comment.setCreated(LocalDateTime.now());
+        em.persist(comment);
 
-        List<Item> result = repository.findByOwnerOrderByIdPageable(user.getId(), pageable);
+        List<Comment> result = repository.findByItemIn(List.of(item.getId()), Sort.by(DESC, "created"));
 
         assertThat(result, hasSize(1));
         assertThat(result.get(0).getId(), notNullValue());
-        assertThat(result.get(0).getName(), equalTo(item.getName()));
-        assertThat(result.get(0).getDescription(), equalTo(item.getDescription()));
-        assertThat(result.get(0).getAvailable(), equalTo(item.getAvailable()));
-        assertThat(result.get(0).getOwner(), equalTo(item.getOwner()));
+        assertThat(result.get(0).getText(), equalTo(comment.getText()));
+        assertThat(result.get(0).getItem(), equalTo(comment.getItem()));
+        assertThat(result.get(0).getAuthor(), equalTo(comment.getAuthor()));
+        assertThat(result.get(0).getCreated(), equalTo(comment.getCreated()));
     }
 }
